6e89e4bf7bf5f0b38195eaa1118b7e58ed627433
pchevieux <p.chevieux@aristid.com>
2023-08-17 16:14:06 +0200

    docs(Input) : Replace Canvas bu CustomCanvas to display source code

---
+++ stories/02-Design System/04-DataEntry/Input/exemples/clearicon.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {KitInput} from '@kit/DataEntry/';
+import {KitDivider, KitSpace} from '@kit/Layout/';
+
+const App = ({}) => {
+    return (
+        <KitSpace direction="vertical">
+            <KitDivider orientation="left">Input</KitDivider>
+            <KitSpace align="start">
+                <KitInput placeholder="Input" />
+                <KitInput placeholder="Input" allowClear={false} />
+            </KitSpace>
+            <KitDivider orientation="left">TextArea</KitDivider>
+            <KitSpace align="start">
+                <KitInput.TextArea placeholder="TextArea" />
+                <KitInput.TextArea placeholder="TextArea" allowClear={false} />
+            </KitSpace>
+            <KitDivider orientation="left">Password</KitDivider>
+            <KitSpace align="start">
+                <KitInput.Password />
+                <KitInput.Password allowClear={false} />
+            </KitSpace>
+        </KitSpace>
+    );
+};
+
+export default App;
---
+++ stories/02-Design System/04-DataEntry/Input/exemples/index.tsx
@@ -0,0 +1,42 @@
+import Basic from './basic';
+import Prefixsuffix from './prefixsuffix';
+import Password from './password';
+import Textarea from './textarea';
+import Autosizing from './autosizing';
+import Counting from './counting';
+import Clearicon from './clearicon';
+import Status from './status';
+import Wrapper from './wrapper';
+
+import BasicSource from './basic?raw';
+import PrefixsuffixSource from './prefixsuffix?raw';
+import PasswordSource from './password?raw';
+import TextareaSource from './textarea?raw';
+import AutosizingSource from './autosizing?raw';
+import CountingSource from './counting?raw';
+import CleariconSource from './clearicon?raw';
+import StatusSource from './status?raw';
+import WrapperSource from './wrapper?raw';
+
+export const Sources = {
+    Basic: BasicSource,
+    Prefixsuffix: PrefixsuffixSource,
+    Password: PasswordSource,
+    Textarea: TextareaSource,
+    Autosizing: AutosizingSource,
+    Counting: CountingSource,
+    Clearicon: CleariconSource,
+    Status: StatusSource,
+    Wrapper: WrapperSource
+};
+export default {
+    Basic,
+    Prefixsuffix,
+    Password,
+    Textarea,
+    Autosizing,
+    Counting,
+    Clearicon,
+    Status,
+    Wrapper
+};
