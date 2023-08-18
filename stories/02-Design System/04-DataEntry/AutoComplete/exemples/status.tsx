1e55e855d81902e2065f87793106c4eff9a3d047
pchevieux <p.chevieux@aristid.com>
2023-08-18 11:18:59 +0200

    docs(AutoComplete) : Replace Canvas by CustomCanvas to display source code

---
+++ stories/02-Design System/04-DataEntry/AutoComplete/exemples/status.tsx
@@ -0,0 +1,44 @@
+import React, {ReactNode, useState} from 'react';
+import {KitAutoComplete} from '@kit/DataEntry';
+import {KitSpace} from '@kit/Layout';
+
+const mockVal = (str, repeat = 1) => ({
+    value: str.repeat(repeat),
+    label: str.repeat(repeat)
+});
+
+const App = ({}) => {
+    const [options, setOptions] = useState<{value: string; label: string}[]>([]);
+    const [anotherOptions, setAnotherOptions] = useState<{value: string; label: string}[]>([]);
+
+    const getPanelValue = searchText =>
+        !searchText ? [] : [mockVal(searchText), mockVal(searchText, 2), mockVal(searchText, 3)];
+
+    return (
+        <KitSpace
+            direction="vertical"
+            style={{
+                width: '100%'
+            }}
+        >
+            <KitAutoComplete
+                options={options}
+                onSearch={text => setOptions(getPanelValue(text))}
+                status="error"
+                style={{
+                    width: 200
+                }}
+            />
+            <KitAutoComplete
+                options={anotherOptions}
+                onSearch={text => setAnotherOptions(getPanelValue(text))}
+                status="warning"
+                style={{
+                    width: 200
+                }}
+            />
+        </KitSpace>
+    );
+};
+
+export default App;
