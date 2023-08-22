ee046df1a8c97a281e005f550b864ff8a9ae2be3
pchevieux <p.chevieux@aristid.com>
2023-08-22 15:20:25 +0200

    docs(Select) : Replace Canvas by CustomCanvas to display source code and add exemple for wrapper

---
+++ stories/02-Design System/04-DataEntry/Select/exemples/disabled.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {KitSelect} from '@kit/DataEntry/';
+import {KitSpace} from '@kit/Layout/';
+import theme from '@theme/index';
+
+const App = ({}) => {
+    const colorOptions = [
+        {
+            value: 'not-allowed',
+            color: theme.color.secondary.red.red400,
+            label: 'Not allowed',
+            disabled: true
+        },
+        {
+            value: 'denied',
+            color: theme.color.secondary.yellow.yellow400,
+            label: 'Denied',
+            disabled: true
+        },
+        {
+            value: 'todo',
+            color: theme.color.primary.blue400,
+            label: 'Todo by agency'
+        },
+        {
+            value: 'ok',
+            color: theme.color.secondary.green.green400,
+            label: 'Ok done by agency'
+        }
+    ];
+
+    const style = {width: 320};
+
+    return (
+        <KitSpace>
+            <KitSelect
+                placeholder="Select a status"
+                style={style}
+                options={colorOptions}
+                defaultActiveFirstOption={false}
+            />
+            <KitSelect placeholder="Select a status" style={style} disabled options={colorOptions} />
+        </KitSpace>
+    );
+};
+
+export default App;
---
+++ stories/02-Design System/04-DataEntry/Select/exemples/wrapper.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {KitSelect} from '@kit/DataEntry/';
+import {KitSpace} from '@kit/Layout/';
+import theme from '@theme/index';
+
+const App = ({}) => {
+    const colorOptions = [
+        {
+            value: 'not-allowed',
+            color: theme.color.secondary.red.red400,
+            label: 'Not allowed'
+        },
+        {
+            value: 'denied',
+            color: theme.color.secondary.yellow.yellow400,
+            label: 'Denied'
+        },
+        {
+            value: 'todo',
+            color: theme.color.primary.blue400,
+            label: 'Todo by agency'
+        },
+        {
+            value: 'ok',
+            color: theme.color.secondary.green.green400,
+            label: 'Ok done by agency'
+        }
+    ];
+
+    const style = {width: 320};
+
+    return (
+        <KitSpace direction="vertical">
+            <KitSelect
+                label="Label"
+                helper="Helper"
+                placeholder="Select a status"
+                style={style}
+                options={colorOptions}
+                defaultActiveFirstOption={false}
+            />
+            <KitSelect
+                label="Label"
+                helper="Helper"
+                placeholder="Select a status"
+                disabled
+                style={style}
+                options={colorOptions}
+                defaultActiveFirstOption={false}
+            />
+            <KitSelect
+                label="Label"
+                helper="Helper"
+                placeholder="Select a status"
+                style={style}
+                options={colorOptions}
+                defaultActiveFirstOption={false}
+                status="warning"
+            />
+            <KitSelect
+                label="Label"
+                helper="Helper"
+                placeholder="Select a status"
+                style={style}
+                options={colorOptions}
+                defaultActiveFirstOption={false}
+                status="error"
+            />
+        </KitSpace>
+    );
+};
+
+export default App;
