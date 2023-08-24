20b43dcbca269570425ef7b3d5e20854e46f63e2
v.delmotte <v.delmotte@aristid.com>
2023-08-24 15:49:01 +0200

    docs(DropDown) : replace canvas with custom canvas

---
+++ stories/02-Design System/03-Navigation/DropDown/exemples/basic.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {KitDropDown} from '@kit/Navigation';
+import {KitSpace} from '@kit/Layout';
+import {DownOutlined} from '@ant-design/icons';
+
+const items = [
+    {
+        key: '1',
+        label: (
+            <a target="_blank" rel="noopener noreferrer" href="http://www.aristid..com/">
+                1st menu item link
+            </a>
+        )
+    },
+    {
+        key: '2',
+        label: '2nd menu item'
+    },
+    {
+        key: '3',
+        label: '3rd menu item'
+    }
+];
+
+const App = () => {
+    return (
+        <KitDropDown menu={{items}}>
+            <KitSpace>
+                Hover me
+                <DownOutlined />
+            </KitSpace>
+        </KitDropDown>
+    );
+};
+
+export default App;
