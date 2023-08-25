472cf43660d544e634e967bb54a3643f0f20939a
v.delmotte <v.delmotte@aristid.com>
2023-08-25 11:03:15 +0200

    docs(Pagination) : replace canvas with custom canvas

---
+++ stories/02-Design System/03-Navigation/Pagination/exemples/basic.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {KitPagination} from '@kit/Navigation';
+
+const App = () => {
+    return <KitPagination defaultCurrent={1} total={50} />;
+};
+
+export default App;
---
+++ stories/02-Design System/03-Navigation/Pagination/exemples/index.tsx
@@ -0,0 +1,51 @@
+import Basic from './basic';
+import Bordered from './bordered';
+import More from './more';
+import Changer from './changer';
+import Jumper from './jumper';
+import MiniSize from './miniSize';
+import SimpleMode from './simpleMode';
+import Controlled from './controlled';
+import TotalNumber from './totalNumber';
+import ShowAll from './showAll';
+import PrevNext from './prevNext';
+
+import BasicSource from './basic?raw';
+import BorderedSource from './bordered?raw';
+import MoreSource from './more?raw';
+import ChangerSource from './changer?raw';
+import JumperSource from './jumper?raw';
+import MiniSizeSource from './miniSize?raw';
+import SimpleModeSource from './simpleMode?raw';
+import ControlledSource from './controlled?raw';
+import TotalNumberSource from './totalNumber?raw';
+import ShowAllSource from './showAll?raw';
+import PrevNextSource from './prevNext?raw';
+
+export const Sources = {
+    Basic: BasicSource,
+    Bordered: BorderedSource,
+    More: MoreSource,
+    Changer: ChangerSource,
+    Jumper: JumperSource,
+    MiniSize: MiniSizeSource,
+    SimpleMode: SimpleModeSource,
+    Controlled: ControlledSource,
+    TotalNumber: TotalNumberSource,
+    ShowAll: ShowAllSource,
+    PrevNext: PrevNextSource
+};
+
+export default {
+    Basic,
+    Bordered,
+    More,
+    Changer,
+    Jumper,
+    MiniSize,
+    SimpleMode,
+    Controlled,
+    TotalNumber,
+    ShowAll,
+    PrevNext
+};
