0e63ebd402df5607763b9d7cdee3e226cd25c47b
pchevieux <p.chevieux@aristid.com>
2023-08-31 16:17:12 +0200

    fix(Select) : Update box-shadow

--- src/Kit/DataEntry/Select/style.tsx
+++ src/Kit/DataEntry/Select/style.tsx
@@ -85,6 +85,7 @@ export const SelectDropDownStyle = createGlobalStyle`
     }
 
     .kit-select-dropdown-content {
+        box-shadow: 0px 3px 14px 0px rgba(0, 0, 0, 0.30);
         background-color: ${theme.color.secondary.mediumGrey.mediumGrey100};
         padding: 10px;
         overflow: hidden;
