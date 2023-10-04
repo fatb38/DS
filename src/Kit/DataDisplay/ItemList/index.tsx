e5eeca5e754bc23e4dbebca56bad73bf7a452a8f
philippechevieux <p.chevieux@aristid.com>
2023-10-04 13:38:53 +0200

    refacto(ItemList) : Rename isDisabled per disabled

--- src/Kit/DataDisplay/ItemList/index.tsx
+++ src/Kit/DataDisplay/ItemList/index.tsx
@@ -182,7 +182,7 @@ export const KitItemList: FunctionComponent<IKitItemList> = ({
     onSelectChange,
     tagNumber,
     onRafterClick,
-    isDisabled = false,
+    disabled = false,
     onClick,
     className,
     ...props
@@ -226,7 +226,7 @@ export const KitItemList: FunctionComponent<IKitItemList> = ({
             isSelectable && (
                 <div>
                     <KitCheckbox
-                        disabled={isDisabled}
+                        disabled={disabled}
                         onClick={e => e.stopPropagation()}
                         onChange={e => {
                             onSelectChange && onSelectChange(e);
@@ -367,7 +367,7 @@ export const KitItemList: FunctionComponent<IKitItemList> = ({
     const _getClasses = () => {
         let classes = className;
 
-        classes += isDisabled ? ' kit-item-list-disabled' : '';
+        classes += disabled ? ' kit-item-list-disabled' : '';
         classes += isClickable ? ' kit-item-list-clickable' : '';
 
         return classes;
