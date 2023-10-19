e408f9185c5e57e4f479fd4d4012249f34a93139
v.delmotte <v.delmotte@aristid.com>
2023-10-19 14:38:05 +0200

    refacto(Tag) : fix MR comment

--- src/Kit/DataEntry/index.tsx
+++ src/Kit/DataEntry/index.tsx
@@ -4,7 +4,6 @@ export {KitInputNumber} from './InputNumber';
 export {KitRadio} from './Radio';
 export {KitRate} from './Rate';
 export {KitSelect} from './Select';
-export {KitTag} from '../DataDisplay/Tag';
 export {KitSwitch} from './Switch';
 export {KitDatePicker} from './DatePicker';
 export {KitAutoComplete} from './AutoComplete';
