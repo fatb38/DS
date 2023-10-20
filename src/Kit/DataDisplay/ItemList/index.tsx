9cd9d374a89b32adf5f8ee3cf49764b4dbed2ac2
philippechevieux <p.chevieux@aristid.com>
2023-10-20 14:30:49 +0200

    fix(ItemList) : Picture with icon was too small

--- src/Kit/DataDisplay/ItemList/index.tsx
+++ src/Kit/DataDisplay/ItemList/index.tsx
@@ -20,7 +20,7 @@ const StyledItemList = styled.div<IStyledKitItemList>`
     background-color: ${({$theme}) => $theme.itemList.colors.background.default};
     box-shadow: 0px 3px 8px 0px rgba(0, 0, 0, 0.1);
     border: 1px solid ${({$theme}) => $theme.itemList.colors.border.default};
-    border-radius: 7px;
+    border-radius: 8px;
 
     &:hover {
         border: 1px solid ${({$theme}) => $theme.itemList.colors.border.hover};
@@ -51,14 +51,11 @@ const StyledItemList = styled.div<IStyledKitItemList>`
         }
 
         &.kit-item-list-icon {
-            .anticon {
-                font-size: 2rem;
-                text-align: center;
-                width: 100%;
-                height: 100%;
-                display: inline-flex;
-                align-items: center;
-                justify-content: center;
+            .kit-icon {
+                font-size: 1.5rem;
+                width: 34px;
+                height: 34px;
+                border-radius: 8px;
             }
         }
 
