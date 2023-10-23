a6f3863363cb4c257de0d0c4bf37a88dfe3c26cc
v.delmotte <v.delmotte@aristid.com>
2023-10-23 09:40:29 +0200

    refacto : fix mr comments

--- src/Kit/General/Icon/types.d.ts
+++ src/Kit/General/Icon/types.d.ts
@@ -18,6 +18,6 @@ export interface IStyledKitIcon extends Omit<IKitIcon, 'icon'> {
     $on?: boolean;
     $isClickable?: boolean;
     className?: string;
-    $backgroundColor?: string | null;
+    $backgroundColor?: string;
     $color?: string;
 }
