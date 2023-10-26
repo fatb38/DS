faa33566ffb06185d54c5c6ef8a46efe8fef1050
emile <e.voiron-levy@cacom.fr>
2023-10-26 14:54:27 +0200

    add turbosnap plugin for vite

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
 import type {StorybookConfig} from '@storybook/react-vite';
 import remarkGfm from 'remark-gfm';
+import {mergeConfig} from 'vite';
+import turbosnap from 'vite-plugin-turbosnap';
 
 const config: StorybookConfig = {
     stories: ['../stories/!(template)/**/*.mdx', '../stories/!(template)/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -30,6 +32,19 @@ const config: StorybookConfig = {
     docs: {
         autodocs: true,
         docsMode: true
+    },
+    async viteFinal(config, {configType}) {
+        return mergeConfig(config, {
+            plugins:
+                configType === 'PRODUCTION'
+                    ? [
+                          turbosnap({
+                              // This should be the base path of your storybook.  In monorepos, you may only need process.cwd().
+                              rootDir: config.root ?? process.cwd()
+                          })
+                      ]
+                    : []
+        });
     }
 };
 export default config;
