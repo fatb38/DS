d05ef365de7e7b3b890196e0af21f3918dd3495f
Delmotte-Vincent <delmotte-vincent@outlook.fr>
2023-10-31 11:14:53 +0100

    feat(ItemList) : chromatic test

--- stories/02-Design System/05-DataDisplay/ItemList/ItemList.stories.tsx
+++ stories/02-Design System/05-DataDisplay/ItemList/ItemList.stories.tsx
@@ -1,6 +1,10 @@
 import type {Meta, StoryObj} from '@storybook/react';
-import {KitItemList} from '@kit/DataDisplay';
+import {KitImage, KitItemList} from '@kit/DataDisplay';
 import {argTypes, Template} from './data';
+import React from 'react';
+import {KitIcon} from '@kit/General';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faUser} from '@fortawesome/free-regular-svg-icons';
 
 const meta: Meta<typeof KitItemList> = {
     component: KitItemList,
@@ -15,5 +19,71 @@ export const Api: Story = {
     render: Template,
     parameters: {
         chromatic: {disableSnapshot: true}
+    }
+};
+
+export const Basic: Story = {
+    render: () => {
+        return (
+            <div style={{width: '350px', display: 'flex', flexDirection: 'column', gap: '20px'}}>
+                <KitItemList
+                    onSelectChange={e => console.log('selected', e.target.checked)}
+                    picture={<KitIcon icon={<FontAwesomeIcon icon={faUser} />} />}
+                    title="Tondeuse à gazon"
+                    description="Tondeuse thermique Auto tractée 70 VL 55 TH"
+                    tagNumber={4}
+                    onRafterClick={() => console.log('click rafter')}
+                    onClick={() => console.log('on click itemlist')}
+                />
+
+                <KitItemList
+                    onSelectChange={e => console.log('selected', e.target.checked)}
+                    picture={<KitImage src="public/images/tondeuse.png" />}
+                    title="Tondeuse à gazon"
+                    description="Tondeuse thermique Auto tractée 70 VL 55 TH"
+                    tagNumber={4}
+                    onRafterClick={() => console.log('click rafter')}
+                    onClick={() => console.log('on click itemlist')}
+                />
+            </div>
+        );
+    },
+
+    parameters: {
+        chromatic: {disableSnapshot: false}
+    }
+};
+
+export const Disabled: Story = {
+    render: () => {
+        return (
+            <div style={{width: '350px', display: 'flex', flexDirection: 'column', gap: '20px'}}>
+                <KitItemList
+                    onSelectChange={e => console.log('selected', e.target.checked)}
+                    picture={<KitIcon icon={<FontAwesomeIcon icon={faUser} />} />}
+                    title="Tondeuse à gazon"
+                    description="Tondeuse thermique Auto tractée 70 VL 55 TH"
+                    tagNumber={4}
+                    onRafterClick={() => console.log('click rafter')}
+                    onClick={() => console.log('on click itemlist')}
+                    disabled
+                />
+
+                <KitItemList
+                    onSelectChange={e => console.log('selected', e.target.checked)}
+                    picture={<KitImage src="public/images/tondeuse.png" />}
+                    title="Tondeuse à gazon"
+                    description="Tondeuse thermique Auto tractée 70 VL 55 TH"
+                    tagNumber={4}
+                    onRafterClick={() => console.log('click rafter')}
+                    onClick={() => console.log('on click itemlist')}
+                    disabled
+                />
+            </div>
+        );
+    },
+
+    parameters: {
+        chromatic: {disableSnapshot: false}
     }
 };
