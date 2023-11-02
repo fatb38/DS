fca6d937efc7f6d104d27a16e7197e953b89fa38
Delmotte-Vincent <delmotte-vincent@outlook.fr>
2023-11-02 08:13:05 +0100

    feat(Switch) : chromatic test

--- stories/02-Design System/04-DataEntry/Switch/Switch.stories.tsx
+++ stories/02-Design System/04-DataEntry/Switch/Switch.stories.tsx
@@ -1,6 +1,7 @@
 import type {Meta, StoryObj} from '@storybook/react';
 import {KitSwitch} from '@kit/DataEntry';
 import {argTypes, Template} from './data';
+import React from 'react';
 
 const meta: Meta<typeof KitSwitch> = {
     component: KitSwitch,
@@ -15,5 +16,27 @@ export const Api: Story = {
     render: Template,
     parameters: {
         chromatic: {disableSnapshot: true}
+    }
+};
+
+export const Basic: Story = {
+    render: () => (
+        <div style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
+            <div style={{display: 'flex', gap: '20px'}}>
+                <KitSwitch />
+                <KitSwitch defaultChecked />
+            </div>
+            <div style={{display: 'flex', gap: '20px'}}>
+                <KitSwitch loading />
+                <KitSwitch defaultChecked loading />
+            </div>
+            <div style={{display: 'flex', gap: '20px'}}>
+                <KitSwitch disabled />
+                <KitSwitch defaultChecked disabled />
+            </div>
+        </div>
+    ),
+    parameters: {
+        chromatic: {disableSnapshot: false}
     }
 };
