71d71bd5335c08692f3437a83815fd0229689da5
Delmotte-Vincent <delmotte-vincent@outlook.fr>
2023-11-02 08:03:59 +0100

    feat(Upload) : chromatic test

--- stories/02-Design System/04-DataEntry/Upload/Upload.stories.tsx
+++ stories/02-Design System/04-DataEntry/Upload/Upload.stories.tsx
@@ -1,6 +1,9 @@
 import type {Meta, StoryObj} from '@storybook/react';
 import {KitUpload} from '@kit/DataEntry';
 import {argTypes, Template} from './data';
+import React from 'react';
+import {UploadProps} from 'antd';
+import KitDragger from '@kit/DataEntry/Upload/Dragger';
 
 const meta: Meta<typeof KitUpload> = {
     component: KitUpload,
@@ -15,5 +18,109 @@ export const Api: Story = {
     render: Template,
     parameters: {
         chromatic: {disableSnapshot: true}
+    }
+};
+
+export const TypeText: Story = {
+    render: () => {
+        const defaultFileList: UploadProps['defaultFileList'] = [
+            {
+                uid: '1',
+                name: 'xxx.png',
+                status: 'uploading',
+                url: 'http://www.baidu.com/xxx.png',
+                percent: 33
+            },
+            {
+                uid: '2',
+                name: 'yyy.png',
+                status: 'done',
+                url: 'http://www.baidu.com/yyy.png'
+            },
+            {
+                uid: '3',
+                name: 'zzz.png',
+                status: 'error',
+                response: 'Server Error 500',
+                url: 'http://www.baidu.com/zzz.png'
+            }
+        ];
+        return (
+            <div style={{width: '400px'}}>
+                <KitUpload defaultFileList={defaultFileList} listType="text" />{' '}
+            </div>
+        );
+    },
+    parameters: {
+        chromatic: {disableSnapshot: false}
+    }
+};
+
+export const Picture: Story = {
+    render: () => {
+        const defaultFileList: UploadProps['defaultFileList'] = [
+            {
+                uid: '0',
+                name: 'xxx.png',
+                status: 'uploading',
+                percent: 33
+            },
+            {
+                uid: '-1',
+                name: 'yyy.png',
+                status: 'done',
+                url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+                thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+            },
+            {
+                uid: '-2',
+                name: 'zzz.png',
+                status: 'error'
+            }
+        ];
+        return (
+            <div style={{width: '400px'}}>
+                <KitUpload defaultFileList={defaultFileList} listType="picture" />
+            </div>
+        );
+    },
+    parameters: {
+        chromatic: {disableSnapshot: false}
+    }
+};
+
+export const PictureWall: Story = {
+    render: () => {
+        const defaultFileList: UploadProps['defaultFileList'] = [
+            {
+                uid: '-1',
+                name: 'image.png',
+                status: 'done',
+                url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+            },
+            {
+                uid: '-xxx',
+                percent: 50,
+                name: 'image.png',
+                status: 'uploading',
+                url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+            },
+            {
+                uid: '-3',
+                name: 'image.png',
+                status: 'error'
+            }
+        ];
+        return <KitUpload listType="picture-card" fileList={defaultFileList}></KitUpload>;
+    },
+    parameters: {
+        chromatic: {disableSnapshot: false}
+    }
+};
+
+export const Dragger: Story = {
+    render: () => <KitDragger style={{width: '500px'}} multiple />,
+    parameters: {
+        chromatic: {disableSnapshot: false}
     }
 };
