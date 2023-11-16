e243f20adcdc6a58605093b64e8aa78c7afd5e86
Delmotte-Vincent <delmotte-vincent@outlook.fr>
2023-11-16 14:14:11 +0100

    refacto(Tour) : fix PR comment

--- src/Kit/DataDisplay/Tour/index.tsx
+++ src/Kit/DataDisplay/Tour/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import {Tour} from 'antd';
 import {FunctionComponent} from 'react';
-import {styled} from 'styled-components';
 import {IKitTour} from './types';
 
-const StyledTour = styled(Tour)``;
-
 export const KitTour: FunctionComponent<IKitTour> = ({...tourProps}) => {
-    return <StyledTour {...tourProps} />;
+    return <Tour {...tourProps} />;
 };
