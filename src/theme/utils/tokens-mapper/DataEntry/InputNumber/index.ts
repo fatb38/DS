a5c4cc54bad2d246b8bf32d2679c97a848218e89
Arnaud G <arno.tgh@gmail.com>
2023-12-13 14:27:58 +0100

    [fix] colorpicker text color

--- src/theme/utils/tokens-mapper/DataEntry/InputNumber/index.ts
+++ src/theme/utils/tokens-mapper/DataEntry/InputNumber/index.ts
@@ -10,7 +10,7 @@ export const inputNumberKitTokenToAntdToken = (kitInputNumberTheme: IKitInputNum
     colorPrimary: `var(${kitInputNumberCssTokens.colors.border.hover}, var(${kitColorsPaletteCssTokens.primary.primary400}))`,
     colorPrimaryHover: `var(${kitInputNumberCssTokens.colors.border.hover}, var(${kitColorsPaletteCssTokens.primary.primary400}))`,
     colorTextPlaceholder: `var(${kitInputNumberCssTokens.colors.typography.placeholder.default}, var(${kitColorsPaletteCssTokens.secondary.mediumGrey.mediumGrey500}))`,
-    colorText: `var(${kitInputNumberCssTokens.colors.typography.content.default}, var(${kitColorsPaletteCssTokens.secondary.mediumGrey.mediumGrey300}))`,
+    colorText: `var(${kitInputNumberCssTokens.colors.typography.content.default}, var(${kitColorsPaletteCssTokens.primary.primary300}))`,
     borderRadius: kitInputNumberTheme.border.radius,
     fontFamily: `var(${kitInputNumberCssTokens.typography.fontFamily}, var(${typographyCssTokens.fontFamily}))`,
     fontSize: kitInputNumberTheme.typography.fontSize
