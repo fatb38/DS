9656a26faa3c6b6e2769fd2233488c0499e0ada8
philippechevieux <philippe.chevieux@gmail.com>
2024-01-16 14:32:14 +0100

    test(Collapse): update properties

--- stories/02-Design System/05-DataDisplay/Collapse/Collapse.stories.tsx
+++ stories/02-Design System/05-DataDisplay/Collapse/Collapse.stories.tsx
@@ -29,10 +29,86 @@ export const ChromaticTest: Story = {
             <div style={{width: '600px'}}>
                 <SizeCollapseTest />
             </div>
-            <div style={{width: '600px'}}>
-                <BasicCollapseTest />
-                <CustomHeaderCollapseTest />
+        );
+    },
+    parameters: {
+        chromatic: {disableSnapshot: false}
+    }
+};
+
+export const Size: Story = {
+    render: () => {
+        const items = [
+            {
+                key: '1',
+                label: 'This is panel header 1',
+                children: <p>{text}</p>
+            }
+        ];
+        return (
+            <div style={{width: '100%', display: 'flex', flexDirection: 'column', gap: '40px'}}>
+                <div style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
+                    <KitCollapse size="small" items={items} />
+                    <KitCollapse collapsible="disabled" size="small" items={items} />
+                </div>
+                <div style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
+                    <KitCollapse items={items} />
+                    <KitCollapse collapsible="disabled" items={items} />
+                </div>
+                <div style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
+                    <KitCollapse size="large" items={items} />
+                    <KitCollapse collapsible="disabled" size="large" items={items} />
+                </div>
             </div>
         </div>
     )
+};
+
+export const CustomHeader: Story = {
+    render: () => {
+        const items = [
+            {
+                key: '1',
+                label: (
+                    <KitCollapse.Header
+                        imageSrc="/public/images/catalog.jpg"
+                        title="Title 2"
+                        description="This is a description 2"
+                        tagContent={'sprint #6'}
+                        onSwitchChange={checked => console.log(checked)}
+                    />
+                ),
+                children: <p>{text}</p>,
+                isActive: true,
+                extra: (
+                    <KitCollapse.HeaderExtra
+                        actions={[
+                            {
+                                icon: <FontAwesomeIcon icon={faPlus} />,
+                                label: 'Add',
+                                onClick: () => console.log('onClick: Add')
+                            },
+                            {
+                                icon: <FontAwesomeIcon icon={faMinus} />,
+                                label: 'Subtract',
+                                onClick: () => console.log('onClick: Subtract')
+                            }
+                        ]}
+                    />
+                )
+            }
+        ];
+
+        return (
+            <div style={{width: '100%', display: 'flex', flexDirection: 'column', gap: '20px'}}>
+                <KitCollapse items={items} />
+                <KitCollapse items={items} activeKey="1" />
+                <KitCollapse collapsible="disabled" items={items} />
+            </div>
+        );
+    },
+
+    parameters: {
+        chromatic: {disableSnapshot: false}
+    }
 };
